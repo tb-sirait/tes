cb370d6554d5f6ec9cc367088f6a0a421880dbba
tb-sirait <tbsintheworld@gmail.com>
2025-07-30 15:23:53 +0700

    update firebaseconfig

--- porto_icp/src/firebaseConfig.js
+++ porto_icp/src/firebaseConfig.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  databaseURL: "https://taor-3e735-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  databaseURL: "https://porto-app-web-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
 const app = initializeApp(firebaseConfig);
